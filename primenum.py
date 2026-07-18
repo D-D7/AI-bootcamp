4c4d30c468f91c07081f3aaec53b8525e5c5cae1
D-D7 <memyselfanjanagrg@gmail.com>
2026-07-18 19:43:32 +0545

    primenum.py

--- primenum.py
+++ primenum.py
@@ -1,1 +1,11 @@
+from math import sqrt
 
+def prime(n, i):
+    if i == 1:
+        return True
+    if n % i == 0:
+        return False
+    return prime(n, i - 1)
+
+n = 13
+print(prime(n, int(sqrt(n))))
